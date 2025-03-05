26e736f2936cdbbb93d7d68d27a91aadd32fc1c0
Suzair0318 <khansuzair1@gmail.com>
2025-03-05 15:05:15 +0500

    deploy gmail

--- src/utils/gmail_transport.js
+++ src/utils/gmail_transport.js
@@ -2,11 +2,25 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 // Nodemailer Setup
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.GMAIL_SECRET_EMIAL, // Your Gmail
+//         pass: process.env.GMAIL_SECRET_PASS // App Password
+//     }
+// });
+
+// Nodemailer Setup
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,  // ✅ Change port from 465 to 587
+    secure: false,  // ❌ 465 ke liye `true`, lekin 587 ke liye `false`
     auth: {
         user: process.env.GMAIL_SECRET_EMIAL, // Your Gmail
         pass: process.env.GMAIL_SECRET_PASS // App Password
+    },
+    tls: {
+        rejectUnauthorized: false // ✅ Ignore self-signed cert errors
     }
 });
 
