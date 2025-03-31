ecc239331458dad741851b1676dbb31f7e2a7975
root <root@ubuntu-cloudpanel.localhost>
2025-03-31 15:46:27 +0000

    new latency code error  resolve

--- src/test/routes.js
+++ src/test/routes.js
@@ -11,14 +11,13 @@ const ServerTest = require("./models/test_models");
 async function getIPAndRegion(req) {
     try {
         let clientIP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
+          
         // Remove extra formatting if any (useful in some cases)
         clientIP = clientIP.split(",")[0].trim();
-
         // First Try: Offline Lookup using `geoip-lite`
         // "39.41.235.12"
         const geo = geoip.lookup(clientIP);
-
+        
         if (geo && (geo.city || geo.country)) {
             const city = geo.city || "Unknown City";
             const country = geo.country || "Unknown Country";
@@ -37,14 +36,18 @@ async function getIPAndRegion(req) {
 
 
 routes.post('/test_latency' , async(req ,res) => {
-     
+     try {
+      const start = Date.now(); 
     const { ip, region } = await getIPAndRegion(req);
         if (!ip || ip === "Unknown"){
             res.status(400).json({ error: "Could not determine IP" });
         }  
-    try {
-        const latencyResult = await ping.promise.probe(ip);
-        const latency = latencyResult.time;
+       
+           // HTTP Request for Latency Measurement
+         await axios.get(`http://69.62.125.138/client/test`);  // Timeout added
+        
+        const latency = Date.now() - start; // Calculate latency
+         
 
         const newTest = new ServerTest({ ip, region, latency });
         await newTest.save();
