e812e3b79f982fbc3a8890088e2b483509db53b9
Suzair0318 <khansuzair1@gmail.com>
2025-07-15 16:29:26 +0500

    Value Change

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.use(
     })
 );
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '2000mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2000mb' }));
 
 connectDB();
 
